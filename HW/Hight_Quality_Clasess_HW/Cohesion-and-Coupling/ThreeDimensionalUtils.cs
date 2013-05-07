5bbaf1adb96375864747ffdc3f714a1a6a7d6a04
krstan4o <krstan4o@yahoo.com>
2013-05-08 02:02:24 +0300

    kjb

---
+++ HW/Hight_Quality_Clasess_HW/Cohesion-and-Coupling/ThreeDimensionalUtils.cs
@@ -0,0 +1,46 @@
+﻿namespace CohesionAndCoupling
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+
+    public class ThreeDimensionalUtils
+    {
+        /// <summary>
+        /// Calculates distance between two points in 3D space.
+        /// </summary>
+        /// <param name="x1">First point x axis.</param>
+        /// <param name="y1">First point y axis.</param>
+        /// <param name="z1">First point z axis.</param>
+        /// <param name="x2">Second point x axis.</param>
+        /// <param name="y2">Second point y axis.</param>
+        /// <param name="z2">Second point z axis.</param>
+        /// <returns>The calculated distance as double value.</returns>
+        public static double CalcDistance3D(double x1, double y1, double z1, double x2, double y2, double z2)
+        {
+            double distance = Math.Sqrt(((x2 - x1) * (x2 - x1)) + ((y2 - y1) * (y2 - y1)) + ((z2 - z1) * (z2 - z1)));
+            return distance;
+        }
+
+        /// <summary>
+        /// Calculates the 3D diagonal of figure with width, height and depth.
+        /// <seealso cref="http://calculus-geometry.hubpages.com/hub/How-to-Find-the-Diagonal-of-a-Rectangular-Box-Rectangular-Prism-with-Examples"/>
+        /// </summary>
+        /// <param name="width">The width of the figure.</param>
+        /// <param name="height">The height of the figure.</param>
+        /// <param name="depth">The depth of the figure.</param>
+        /// <returns>The calculated diagonal XYZ as double value.</returns>
+        public static double CalcDiagonalXYZ(double width, double height, double depth)
+        {
+            double distance = Math.Sqrt((width * width) + (height * height) + (depth * depth));
+            return distance;
+        }
+
+        public static double CalcVolume(double width, double height, double depth)
+        {
+            double volume = width * height * depth;
+            return volume;
+        }
+    }
+}
---
+++ HW/Hight_Quality_Clasess_HW/Cohesion-and-Coupling/TwoDimensionalUtils.cs
@@ -0,0 +1,33 @@
+﻿namespace CohesionAndCoupling
+{
+    using System;
+
+    public static class TwoDimensionalUtils
+    {
+        /// <summary>
+        /// Calculates distance between two points in 2D space.
+        /// </summary>
+        /// <param name="x1">First point x axis.</param>
+        /// <param name="y1">First point y axis.</param>
+        /// <param name="x2">Second point x axis.</param>
+        /// <param name="y2">Second point x axis.</param>
+        /// <returns></returns>
+        public static double CalcDistance2D(double x1, double y1, double x2, double y2)
+        {
+            double distance = Math.Sqrt(((x2 - x1) * (x2 - x1)) + ((y2 - y1) * (y2 - y1)));
+            return distance;
+        }
+
+        /// <summary>
+        /// Calculates diagonal of figure in 2D space.
+        /// </summary>
+        /// <param name="firstSide">First side of the figure.</param>
+        /// <param name="secondSide">Second side of the figure.</param>
+        /// <returns>The calculated diagonal as double value.</returns>
+        public static double CalcDiagonal(double firstSide, double secondSide)
+        {
+            double distance = Math.Sqrt((firstSide * firstSide) + (secondSide * secondSide));
+            return distance;
+        }
+    }
+}
